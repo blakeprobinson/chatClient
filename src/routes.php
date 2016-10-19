276a922078fc5761d859db3dbec1cf4e04946ec7
Blake Robinson <blakerobinson@Blakes-MacBook-Pro.local>
2016-10-19 07:03:14 -0400

    initial commit

---
+++ src/routes.php
@@ -0,0 +1,33 @@
+<?php
+// Routes
+
+$app->get('/[{name}]', function ($request, $response, $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/' route");
+    $uri = $request->getUri();
+    $username = $uri->getPath();
+    $message = $username;
+    $ip = $username;
+
+    $statement = $this->db->query("INSERT INTO messages
+        VALUES (NULL, '{$username}', '{$message}', '{$ip}', NOW())");
+        var_dump($statement);
+    if(!$statement) {
+      var_dump($statement);
+      die();
+    }
+
+
+    // Render index view
+    return $this->renderer->render($response, 'index.phtml', $args);
+});
+
+
+// public function addMessage($username, $message, $ip)
+//   {
+//       $username = addslashes($username);
+//       $message = addslashes($message);
+//
+//       return (bool) $this->db->query("INSERT INTO messages
+//           VALUES (NULL, '{$username}', '{$message}', '{$ip}', NOW())");
+//   }
