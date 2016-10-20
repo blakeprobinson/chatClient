7cfd4330cf0344fae7c54474bbeb4c4a1490ba43
Blake Robinson <blakerobinson@Blakes-MacBook-Pro.local>
2016-10-20 18:04:33 -0400

    created route to see if any users are online

--- src/routes.php
+++ src/routes.php
@@ -13,25 +13,23 @@ $app->get('/admin', function ($request, $response, $args) {
 
 //chat route
 
-$app->post('/api/message', function ($request, $response, $args) {
+$app->post('/api/message', function($request, $response, $args) {
 
-    $contents;
-    $body = $request->getParsedBody();
-    $ip = 'test';
+      $body = $request->getParsedBody();
+      $ip = 'test';
 
-    try {
-        $sql = "INSERT INTO messages VALUES (NULL, :username, :message, :ip, NOW())";
-        $stmt = $this->db->prepare($sql);
-        $stmt->bindParam(':username', $body['username']);
-        $stmt->bindParam(':message', $body['text']);
-        $stmt->bindParam(':ip', $ip);
-        $stmt->execute();
+      try {
+          $sql = "INSERT INTO messages VALUES (NULL, :username, :message, :ip, NOW())";
+          $stmt = $this->db->prepare($sql);
+          $stmt->bindParam(':username', $body['username']);
+          $stmt->bindParam(':message', $body['text']);
+          $stmt->bindParam(':ip', $ip);
+          $stmt->execute();
 
-    } catch (PDOException $pdoException) {
-        // Do something with your error message, for now you could just:
-         echo 'exception reached';
-    }
-    return $response;
+      } catch (PDOException $pdoException) {
+           echo $pdoException->getMessage();
+      }
+      return $response;
 });
 
 $app->post('/api/user', function ($request, $response, $args) {
@@ -57,26 +55,33 @@ $app->post('/api/user', function ($request, $response, $args) {
     return $response;
 });
 
-// $app->get('/api/getmessages', function ($request, $response, $args) {
-//
-//     $contents;
-//     $body = $request->getParsedBody();
-//     $name = $body['username'];
-//     $email = $body['email'];
-//     $online = 1;
-//     //SELECT * FROM messages
-//             //ORDER BY `date` DESC
-//     try {
-//         $sql = "INSERT INTO users VALUES (NULL, :name, :email, :online, NOW())";
-//         $stmt = $this->db->prepare($sql);
-//         $stmt->bindParam(':name', $name);
-//         $stmt->bindParam(':email', $email);
-//         $stmt->bindParam(':online', $online);
-//         $stmt->execute();
-//
-//     } catch (PDOException $pdoException) {
-//         // Do something with your error message, for now you could just:
-//          echo $pdoException->getMessage();
-//     }
-//     return $response;
-// });
+$app->get('/api/getmessages', function ($request, $response, $args) {
+
+    $data;
+    $newResponse;
+    try {
+        $sql = "SELECT name, email FROM users
+              WHERE online = 1 LIMIT 1";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute();
+        $user = $stmt->fetch();
+        if(!$user) {
+          $data = array('username' => $user->name, 'email'=> $user->email, 'id'=> $user->id);
+          $newResponse = $response->withJson($data);
+          return $newResponse;
+        } else {
+          $data = array('username' => "", 'email'=> "", 'id'=> "$user->id");
+          $newResponse = $response->withJson($data);
+          return $newResponse;
+        }
+
+        //send response back with username, email, user_id
+        // if(count($result) == 1) {
+        //
+        // }
+
+    } catch (PDOException $pdoException) {
+        // Do something with your error message, for now you could just:
+         echo $pdoException->getMessage();
+    }
+});
